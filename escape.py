74c992628c2aacfbb31fb499f2a5623f04adf9f1
HassanEr-Laptop17 <herrafii80@gmail.com>
2025-06-17 20:00:18 -0400

    Set up basic navigation loop using directions N/S/E/W 6/17/25 8:00pm

--- escape.py
+++ escape.py
@@ -22,7 +22,8 @@ current_room = 'Break Room'
 print("****************************************************************************")
 print("|                                                                          |")
 print("|   Welcome to Factory Shutdown Escape                                     |")
-print("|   To Move between rooms, type: go North, go South, go East, or go West   |")
+print("|   To Move between rooms use these Commands:                              |")
+print("|       N = Go North     S = Go South     E = Go East     W = Go West      |")
 print("|   Type 'exit' to quit the game                                           |")
 print("|                                                                          |")
 print("***************************************************************************")
@@ -30,41 +31,22 @@ print("*************************************************************************
 
 # Escape game loop  
 while current_room != 'exit':
-    print("You are in the", current_room)
-    command = input("Enter your move: ").strip()
+    print("You are in the", current_room,"\n")
+    command = input("Enter your move (N/S/E/W or exit): ").strip().lower()
 
     # The exit command to quit the game
     if command.lower() == 'exit':
         current_room = 'exit'
         print("Thanks for playing. Goodbye :)")
-    # The move commands to navigate between rooms
-    elif command.lower().startswith('go north'):
-        if 'North' in rooms[current_room]:
-            current_room = rooms[current_room]['North']
-        else:
-            print("You can't go that way!\n")
-    elif command.lower().startswith('go south'):
-        if 'South' in rooms[current_room]:
-            current_room = rooms[current_room]['South']
-        else:
-            print("You can't go that way!\n")
-    elif command.lower().startswith('go east'):
-        if 'East' in rooms[current_room]:
-            current_room = rooms[current_room]['East']
-        else:
-            print("You can't go that way!\n")
-    elif command.lower().startswith('go west'):
-        if 'West' in rooms[current_room]:
-            current_room = rooms[current_room]['West']
-        else:
-            print("You can't go that way!\n")
-    elif command.lower().startswith('go '):
-        direction = command[3:].capitalize()
 
+    # The move commands to navigate between rooms
+    elif command in ['n', 's', 'e', 'w']:
+        direction = {'n': 'North', 's': 'South', 'e': 'East', 'w': 'West'}[command]
         if direction in rooms[current_room]:
             current_room = rooms[current_room][direction]
         else:
             print("You can't go that way!\n")
-    # If user enter invalid input
+
+    # If the user enter invalid input
     else:
-        print("Invalid command. Please type something like 'go North' or 'exit'.\n")
+        print("Invalid command. Please type N, S, E, W, or exit.\n")
